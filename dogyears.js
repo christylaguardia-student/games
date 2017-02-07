1593ea6eb6359fe7d45d164352fcea139b5a3dfc
christylaguardia <christinelaguardia@gmail.com>
2017-02-06 20:26:33 -0800

    Add dog year js

--- dogyears.js
+++ dogyears.js
@@ -1,10 +1,10 @@
-var myAge = document.getElementById('userAge').value;
-var firstYears = (2 * 10.5);
-var laterYears = (myAge - 2) * 4;
-var myAgeInDogYears = firstYears + laterYears;
-
+// var myAge = document.getElementById('userAge').value;
 function playGame(){
+  var myAge = prompt('What is your age in human years?');
+  var firstYears = (2 * 10.5);
+  var laterYears = (myAge - 2) * 4;
+  var myAgeInDogYears = firstYears + laterYears;
   console.log('Your age in human years is ' + myAge);
   console.log('Your age in dog years is ' + myAgeInDogYears);
-  document.getElementById('gameResults').textContent = 'You are ' + myAgeInDogYears + ' years old in dog years.';
+  document.getElementById('results').textContent = 'You are ' + myAgeInDogYears + ' years old in dog years.';
 }
